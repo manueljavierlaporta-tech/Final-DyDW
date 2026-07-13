746f455415792ebc3bf62bf0915fc8e83939cd3d
Manuel Javier La Porta <manueljavierlaporta@gmail.com>
2026-07-13 20:37:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,18 @@ let botonDonar = document.getElementById('botonDonar');
 botonDonar.addEventListener('click', () => {
     let body = document.querySelector('body');
     let nuevoDiv = document.createElement('div');
+    nuevoDiv.classList.add('modalOverlay');
     nuevoDiv.innerHTML = `
         <h4>Donación</h4>
         <input type="number" min="0">
     `;
 
-    body.appendChild(nuevoDiv)
-})
+    body.appendChild(nuevoDiv);
+});
+
+document.addEventListener('click', (e) => {
+    if (e.target != botonDonar) {
+        let nuevoDiv = document.querySelector('.modalOverlay');
+        nuevoDiv.remove();
+    }
+});
